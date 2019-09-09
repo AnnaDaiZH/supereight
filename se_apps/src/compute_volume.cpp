04af87694a905b91899f8c944987e53cc83d081b
Sotiris Papatheodorou <sotirisp@protonmail.com>
2019-09-09 15:56:45 +0100

    Add accurate explored volume computation

---
+++ se_apps/src/compute_volume.cpp
@@ -0,0 +1,124 @@
+/**
+ * Copyright (C) 2019 Imperial College London.
+ * Copyright (C) 2019 ETH Zürich.
+ *
+ *
+ *
+ * @file exploration_progress_evaluation.cpp
+ * @author Anna Dai
+ * @author Sotiris Papatheodorou
+ * @date August 24, 2019
+ */
+
+
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include "se/octree.hpp"
+#include "se/post_processing.hpp"
+
+
+
+struct arguments {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  std::string filename;
+  Eigen::Vector3f map_dim;
+};
+
+
+
+struct arguments parse_arguments(int argc, char** argv) {
+  struct arguments args;
+
+  switch (argc) {
+    // Map filename and dimensions supplied.
+    case 5:
+      args.filename = std::string(argv[1]);
+      args.map_dim.x() = atof(argv[2]);
+      args.map_dim.y() = atof(argv[3]);
+      args.map_dim.z() = atof(argv[4]);
+      break;
+
+    // Show usage message.
+    default:
+      std::cout << "Usage: " << argv[0] << " FILENAME DIM_X DIM_Y DIM_Z\n";
+      exit(EXIT_FAILURE);
+  }
+
+  // Test if the file exists.
+  FILE* fp = fopen(args.filename.c_str(), "r");
+  if (fp == nullptr) {
+    std::cout << "Error: file " << args.filename
+        << " does not exist or is not accessible\n";
+    exit(EXIT_FAILURE);
+  } else {
+    fclose(fp);
+  }
+
+  return args;
+}
+
+
+
+int main(int argc, char** argv) {
+  // Parse the input arguments.
+  const struct arguments args = parse_arguments(argc, argv);
+
+  // Initialize the Octree and load the saved map.
+  std::shared_ptr<se::Octree<OFusion> > octree_
+      = std::make_shared<se::Octree<OFusion> >();
+  octree_->load(args.filename);
+
+  // Get and show Octree info.
+  const int   octree_size   = octree_->size();
+  const float octree_dim    = octree_->dim();
+  const float octree_volume = std::pow(octree_dim, 3.f);
+  const float voxel_dim     = octree_->voxelDim();
+  const float voxel_volume  = std::pow(voxel_dim, 3.f);
+  std::cout << "Octree info -----------------------\n";
+  std::cout << "Octree size:   " << octree_size   << " x "
+                                 << octree_size   << " x "
+                                 << octree_size   << " voxels\n";
+  std::cout << "Octree dim:    " << octree_dim    << " x "
+                                 << octree_dim    << " x "
+                                 << octree_dim    << " m\n";
+  std::cout << "Octree volume: " << octree_volume << " m^3\n";
+  std::cout << "Voxel dim:     " << voxel_dim     << " m\n";
+  std::cout << "Voxel volume:  " << voxel_volume  << " m^3\n";
+
+  // Show map info.
+  std::cout << "Map info --------------------------\n";
+  std::cout << args.map_dim.x() << " x "
+            << args.map_dim.y() << " x "
+            << args.map_dim.z() << " m\n";
+
+
+
+  // Count the explored voxels.
+  float  free_voxel_volume;
+  float  occupied_voxel_volume;
+  float  free_node_volume;
+  float  occupied_node_volume;
+  compute_bounded_volume(*octree_, args.map_dim,
+      free_voxel_volume, occupied_voxel_volume,
+      free_node_volume, occupied_node_volume);
+
+
+
+  // Print results.
+  std::cout << "Voxels ----------------------------\n";
+  std::cout << "Explored voxel volume: "
+      << free_voxel_volume + occupied_voxel_volume << " m^3\n";
+  std::cout << "Nodes -----------------------------\n";
+  std::cout << "Explored node volume:  "
+      << free_node_volume + occupied_node_volume << " m^3\n";
+  std::cout << "Results ---------------------------\n";
+  std::cout << "Explored volume:       "
+      << free_node_volume + occupied_node_volume
+      + free_voxel_volume + occupied_voxel_volume << " m^3\n";
+
+  exit(EXIT_SUCCESS);
+}
+
--- se_denseslam/include/se/post_processing.hpp
+++ se_denseslam/include/se/post_processing.hpp
@@ -32,6 +32,7 @@
 #ifndef __POST_PROCESSING_HPP
 #define __POST_PROCESSING_HPP
 
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
 #include <iostream>
@@ -45,9 +46,71 @@
 typedef std::vector<Eigen::Vector3i, Eigen::aligned_allocator<Eigen::Vector3i>> vec3i;
 
 static const Eigen::IOFormat
-    line_fmt (2, 0, " ", " ", "", "", "", "");
+    line_fmt (4, 0, " ", " ", "", "", "", "");
 
 static constexpr float _voxel_state_threshold = 0.f;
+static constexpr float _free_threshold = 0.f;
+static constexpr float _occupied_threshold = 0.f;
+
+
+/*! Return the volume of the intersection of two axis-aligned rectangular
+ * cuboids.
+ *
+ * \note Assuming a_min < a_max and b_min < b_max.
+ *
+ * \note
+ * https://studiofreya.com/3d-math-and-physics/simple-aabb-vs-aabb-collision-detection/
+ */
+float rect_cuboid_intersection_volume(const Eigen::Vector3f& a_min,
+                                      const Eigen::Vector3f& a_max,
+                                      const Eigen::Vector3f& b_min,
+                                      const Eigen::Vector3f& b_max) {
+  const Eigen::Vector3f a_center = (a_max + a_min) / 2.f;
+  const Eigen::Vector3f b_center = (b_max + b_min) / 2.f;
+  const Eigen::Vector3f a_sides = a_max - a_min;
+  const Eigen::Vector3f b_sides = b_max - b_min;
+  const Eigen::Vector3f center_dists
+      = (a_center - b_center).array().abs().matrix();
+  const Eigen::Vector3f limit_dists = (a_sides + b_sides) / 2.f;
+  float volume = 0.f;
+
+  if (   (a_min.x() <= b_min.x()) && (b_max.x() <= a_max.x())
+      && (a_min.y() <= b_min.y()) && (b_max.y() <= a_max.y())
+      && (a_min.z() <= b_min.z()) && (b_max.z() <= a_max.z())) {
+    // a contains b.
+    volume = b_sides.x() * b_sides.y() * b_sides.z();
+
+  } else if ((b_min.x() <= a_min.x()) && (a_max.x() <= b_max.x())
+          && (b_min.y() <= a_min.y()) && (a_max.y() <= b_max.y())
+          && (b_min.z() <= a_min.z()) && (a_max.z() <= b_max.z())) {
+    // b contains a.
+    volume = a_sides.x() * a_sides.y() * a_sides.z();
+
+  } else if ((center_dists.x() < limit_dists.x())
+          && (center_dists.y() < limit_dists.y())
+          && (center_dists.z() < limit_dists.z())) {
+    // a intersects b if they overlap in all 3 axes.
+    Eigen::Vector3f inters_sides = limit_dists - center_dists;
+    // The intersection side cannot be larger than the side of the smallest
+    // cuboid.
+    const Eigen::Vector3f min_sides
+        = (a_sides.array().min(b_sides.array())).matrix();
+    if (inters_sides.x() > min_sides.x())
+      inters_sides.x() = min_sides.x();
+    if (inters_sides.y() > min_sides.y())
+      inters_sides.y() = min_sides.y();
+    if (inters_sides.z() > min_sides.z())
+      inters_sides.z() = min_sides.z();
+
+    volume = inters_sides.x() * inters_sides.y() * inters_sides.z();
+
+  } else {
+    // a and b do not intersect.
+  }
+
+  return volume;
+}
+
 
 
 template<typename T>
@@ -117,7 +180,7 @@ void count_voxels(se::Octree<T>& octree,
 
 
 template<typename T>
-void crop_octree(se::Octree<T>& octree, const Eigen::Vector3f dim) {
+void crop_octree(se::Octree<T>& octree, const Eigen::Vector3f& dim) {
   // Compute the min and max coordinates from the desired dimension. The map is
   // centered on [0 0 0]^T. Offset it so that it's centered inside the octree.
   // Increase the z offset so that the map's floor is halfway on the octree's z
@@ -187,6 +250,116 @@ void crop_octree(se::Octree<T>& octree, const Eigen::Vector3f dim) {
         // Update occupancy and timestamp to unknown.
         node->value_[j].x = _voxel_state_threshold;
         node->value_[j].y = 0.0;
+      }
+    }
+  }
+}
+
+
+
+template<typename T>
+void compute_bounded_volume(se::Octree<T>&         octree,
+                            const Eigen::Vector3f& dim,
+                            float&                 free_voxel_volume,
+                            float&                 occupied_voxel_volume,
+                            float&                 free_node_volume,
+                            float&                 occupied_node_volume) {
+  // Compute the min and max coordinates from the desired dimension. The map is
+  // centered on [0 0 0]^T. Offset it so that it's centered inside the octree.
+  // Increase the z offset so that the map's floor is halfway on the octree's z
+  // axis.
+  const Eigen::Vector3f map_offset (octree.dim() / 2.f, octree.dim() / 2.f,
+      octree.dim() / 2.f + dim.z() / 2.f);
+  const Eigen::Vector3f min_pos_m = -dim / 2.f + map_offset;
+  const Eigen::Vector3f max_pos_m =  dim / 2.f + map_offset;
+
+  //std::cout << dim.format(line_fmt) << " m\n";
+  //std::cout << min_pos_m.format(line_fmt) << " m\n";
+  //std::cout << max_pos_m.format(line_fmt) << " m\n";
+
+  const float voxel_dim     = octree.voxelDim();
+  const float voxel_volume  = std::pow(voxel_dim, 3.f);
+
+
+
+  // Iterate over all voxels.
+  free_voxel_volume = 0.f;
+  occupied_voxel_volume = 0.f;
+  auto &vb_buffer = octree.getBlockBuffer();
+  for (size_t i = 0; i < vb_buffer.size(); ++i) {
+    se::VoxelBlock<T>* vb = vb_buffer[i];
+    // Loop over all the single voxels.
+    const Eigen::Vector3i first = vb->coordinates();
+    const Eigen::Vector3i last = vb->coordinates()
+        + Eigen::Vector3i::Constant(BLOCK_SIDE);
+    for (int z = first.z(); z < last.z(); ++z) {
+      for (int y = first.y(); y < last.y(); ++y) {
+        for (int x = first.x(); x < last.x(); ++x) {
+
+          // Get the child node coordinates.
+          const Eigen::Vector3i child_v (x, y, z);
+          const Eigen::Vector3f child_m_min (
+              voxel_dim * x,
+              voxel_dim * y,
+              voxel_dim * z);
+          const Eigen::Vector3f child_m_max (
+              voxel_dim * (x + 1),
+              voxel_dim * (y + 1),
+              voxel_dim * (z + 1));
+          // Get the current data.
+          const auto val = vb->data(child_v);
+
+          if (val.x < _free_threshold) {
+            // Free voxel.
+            free_voxel_volume += rect_cuboid_intersection_volume(
+                  child_m_min, child_m_max,
+                  min_pos_m, max_pos_m);
+          } else if (val.x > _occupied_threshold) {
+            // Occupied voxel.
+            occupied_voxel_volume += rect_cuboid_intersection_volume(
+                  child_m_min, child_m_max,
+                  min_pos_m, max_pos_m);
+          }
+        }
+      }
+    }
+  }
+
+
+
+  // Iterate over all nodes.
+  free_node_volume = 0.f;
+  occupied_node_volume = 0.f;
+  auto &node_buffer = octree.getNodesBuffer();
+  for (size_t i = 0; i < node_buffer.size(); ++i) {
+    se::Node<T> node = *(node_buffer[i]);
+    // Loop over all the node children.
+    for (size_t j = 0; j < 8; ++j) {
+      // Only consider children that have not been allocated at a lower level.
+      if (node.child(j) == nullptr) {
+        // Get the child node coordinates.
+        const Eigen::Vector3i child_v = node.childCoordinates(j);
+        const unsigned int child_side = node.side_ / 2;
+        const Eigen::Vector3f child_m_min (
+            voxel_dim * child_v.x(),
+            voxel_dim * child_v.y(),
+            voxel_dim * child_v.z());
+        const Eigen::Vector3f child_m_max (
+            voxel_dim * (child_v.x() + child_side),
+            voxel_dim * (child_v.y() + child_side),
+            voxel_dim * (child_v.z() + child_side));
+
+        if (node.value_[j].x < _free_threshold) {
+          // Free node.
+          free_node_volume += rect_cuboid_intersection_volume(
+                child_m_min, child_m_max,
+                min_pos_m, max_pos_m);
+        } else if (node.value_[j].x > _occupied_threshold) {
+          // Occupied node.
+          occupied_node_volume += rect_cuboid_intersection_volume(
+                child_m_min, child_m_max,
+                min_pos_m, max_pos_m);
+        }
       }
     }
   }
