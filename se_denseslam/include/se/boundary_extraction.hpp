78d68f4087ddd12b8c28c5b7eb387a77efb90cd3
Anna Dai <adai@student.ethz.ch>
2019-10-02 15:37:11 +0100

    remove unused frontier update functions

--- se_denseslam/include/se/boundary_extraction.hpp
+++ se_denseslam/include/se/boundary_extraction.hpp
@@ -23,7 +23,7 @@
 
 template<typename T> using Volume = VolumeTemplate<T, se::Octree>;
 
-static inline void insertBlocksToMap(set3i &blocks_map, set3i *blocks) {
+static inline void insertBlocksToSet(set3i &blocks_map, set3i *blocks) {
   if (blocks->size() == 0) return;
   for (auto it = blocks->begin(); it != blocks->end(); ++it) {
     blocks_map.emplace(*it);
@@ -45,43 +45,6 @@ static inline void insertBlocksToMap(set3i &blocks_map, mapvec3i *blocks) {
   for (auto it = blocks->begin(); it != blocks->end(); ++it) {
     blocks_map.emplace(it->first);
   }
-}
-
-/**
- * check if past frontier voxels have been updated and update the std::map
- * [uint64_t morton_code, Eigen::Vector3i coord]
- * Issue map wide function
- */
-template<typename T>
-void updateFrontierMap(const Volume<T> &volume, set3i &blocks_map) {
-//  std::cout << "[supereight] frontier map size: before " << frontier_blocks_map.size()
-//  <<std::endl;
-  se::node_iterator<T> node_it(*(volume._map_index));
-  for (auto it = blocks_map.begin(); it != blocks_map.end(); ) {
-    // check if the occupancy probability of the frontier voxels has been updated
-    // changes voxel states from frontier to free or occupied
-    if (!node_it.deleteFrontierVoxelBlockviaMorton(*it)) {
-      it =blocks_map.erase(it);
-    }else{
-      ++it;
-    }
-  }
-}
-
-template<typename T>
-void updateFrontierMap(const Volume<T> &volume,
-                       set3i &blocks_map,
-                       set3i *blocks,
-                       const bool update_frontier_map) {
-
-  // check if the ones in the map
-  // if (update_frontier_map) {
-    // std::cout << "update frontier map " <<std::endl;
-    // updateFrontierMap(volume, blocks_map);
-  // }
-
-  // insert new frontier blocks to map
-  insertBlocksToMap(blocks_map, blocks);
 }
 
 // level at leaf level
